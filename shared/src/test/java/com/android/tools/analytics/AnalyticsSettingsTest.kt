ab1d3bbef2ffbb57a3380f5d22fd4357649b7abf
Jeffrey van Gogh <jvg@google.com>
2019-03-04 16:04:55 -0800

    user satisfaction survey: don't show again on same day after cancel
    Due to a bug in the evaluation logic, if the user canceled the
    dialog and restarted the IDE on the same day, they would
    be prompted for the dialog again, instead of waiting for 7
    days as was meant to happen.
    
    Test: added
    Fixes: 127355389
    Change-Id: I2a17defda1a6d332f794b5d83f38629fb354aef5

--- shared/src/test/java/com/android/tools/analytics/AnalyticsSettingsTest.kt
+++ shared/src/test/java/com/android/tools/analytics/AnalyticsSettingsTest.kt
@@ -477,6 +477,14 @@ class AnalyticsSettingsTest {
       })
       assertFalse(AnalyticsSettings.shouldRequestUserSentiment())
 
+      // opted in user who was asked today but didn't answer, should not be prompted again today
+      AnalyticsSettings.setInstanceForTest(AnalyticsSettingsData().apply {
+        userId = "db3dd15b-053a-4066-ac93-04c50585edc2"
+        optedIn = true
+        lastSentimentQuestionDate = Date(116, 5, 19)
+      })
+      assertFalse(AnalyticsSettings.shouldRequestUserSentiment())
+
       // opted in user who was asked recently but didn't answer, should be prompted again on a later date
       AnalyticsSettings.setInstanceForTest(AnalyticsSettingsData().apply {
         userId = "db3dd15b-053a-4066-ac93-04c50585edc2"
