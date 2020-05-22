54f31de60f7fe16ffa9c61771a5f876f1a3a7f1b
Ivan Gavrilovic <gavra@google.com>
2020-05-22 17:11:55 +0100

    Envrionment provides access to env vars and system properties
    When accessing analytics from AGP we need to use Gradle APIs in
    order to access system properties and env variables. This CL
    expands Envrionment API to support accessing system properties.
    Any system property read should use this API.
    
    From the AGP side a Gradle-specific implementation of Environment
    is provided and set during initialization.
    
    Bug: 148512507
    Test: existing
    Change-Id: Ic323c445774a6a4e59d05ddddeae2fdb28d0228b

--- publisher/src/main/java/com/android/tools/analytics/GoogleAnalyticsPublisher.kt
+++ publisher/src/main/java/com/android/tools/analytics/GoogleAnalyticsPublisher.kt
@@ -73,7 +73,7 @@ internal constructor(
               CommonMetricsData
                 .majorOsVersion!!)
             .setOsFullVersion(
-              System.getProperty("os.version"))))
+              Environment.instance.getSystemProperty("os.version"))))
     // Set the log source for the Clearcut service. This will be always the
     // same no matter what Android devtool we're logging from.
     .setLogSource(ClientAnalytics.LogRequest.LogSource.ANDROID_STUDIO)
