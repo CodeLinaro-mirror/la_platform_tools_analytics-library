372118ac8a6d3eccb2e219e31897d9557d797955
Alon Albert <aalbert@google.com>
2022-01-26 14:24:43 -0800

    Add a UsageTrackerRule
    Convenience rule for testing UsageTracker.
    
    Bug: N/A
    Test: N/A
    Change-Id: Ieb113af3e6cce5a222209f1f58f2b8f386bd8d73

---
+++ testing/src/main/java/com/android/tools/analytics/UsageTrackerRule.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.analytics
+
+import com.android.testutils.VirtualTimeScheduler
+import org.junit.rules.ExternalResource
+
+/**
+ * A rule that facilitates testing of [UsageTracker]
+ */
+class UsageTrackerRule : ExternalResource() {
+  private val tracker = TestUsageTracker(VirtualTimeScheduler())
+
+  val usages: List<LoggedUsage> get() = tracker.usages
+
+  override fun before() {
+    UsageTracker.setWriterForTest(tracker)
+  }
+
+  override fun after() {
+    UsageTracker.cleanAfterTesting()
+  }
+}
